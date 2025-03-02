f93a3560a50abf9f419a59eb836099cf157f046e
Giovanni Cortazar <manuelgiozar04@gmail.com>
2025-03-01 23:54:51 -0600

    feat: Se implementa y usa directiva @for

--- bases/src/app/dragonball/dragonball.component.ts
+++ bases/src/app/dragonball/dragonball.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 @Component({
   selector: 'app-dragonball',
   imports: [],
@@ -7,5 +12,18 @@ import { Component } from '@angular/core';
   styleUrl: './dragonball.component.css'
 })
 export class DragonballComponent {
+
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 10000 },
+    { id: 2, name: 'Vegeta', power: 9000 },
+    { id: 3, name: 'Gohan', power: 8000 },
+    { id: 4, name: 'Trunks', power: 7000 },
+    { id: 5, name: 'Piccolo', power: 6000 },
+    { id: 6, name: 'Bulma', power: 5000 },
+    { id: 7, name: 'Krillin', power: 4000 },
+    { id: 8, name: 'Picollo', power: 3000 },
+    { id: 9, name: 'Chichi', power: 2000 },
+    { id: 10, name: 'Yamcha', power: 1000 }
+  ]);
 
 }
