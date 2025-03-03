5ea9c0c64ee450b1fd948b28d22a862051d72660
Giovanni Cortazar <manuelgiozar04@gmail.com>
2025-03-03 01:46:32 -0600

    feat: Se implementa LocalStorage

--- bases/src/app/services/dragonball.service.ts
+++ bases/src/app/services/dragonball.service.ts
@@ -1,22 +1,22 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+
+const loadFromLocalStorage = (): Character[] => {
+
+    const characters = localStorage.getItem('characters');
+    return characters ? JSON.parse(characters) : [];
+}
 
 @Injectable({providedIn: 'root'})
 export class DragonballService {
     constructor() { }
 
-    characters = signal<Character[]>([
-        { id: 1, name: 'Goku', power: 10000 },
-        { id: 2, name: 'Vegeta', power: 9000 },
-        { id: 3, name: 'Gohan', power: 8000 },
-        { id: 4, name: 'Trunks', power: 7000 },
-        { id: 5, name: 'Piccolo', power: 6000 },
-        { id: 6, name: 'Bulma', power: 5000 },
-        { id: 7, name: 'Krillin', power: 4000 },
-        { id: 8, name: 'Picollo', power: 3000 },
-        { id: 9, name: 'Chichi', power: 2000 },
-        { id: 10, name: 'Yamcha', power: 1000 }
-      ]);
+    characters = signal<Character[]>(loadFromLocalStorage());
+
+      saveToLocalStorage = effect(() => {
+        // console.log(`Charactter Count: ${this.characters().length}`);
+        localStorage.setItem('characters', JSON.stringify(this.characters()));
+      });
     
       addCharacter(newCharacter: Character) {
         this.characters.update((characters) => [...characters, newCharacter]);
