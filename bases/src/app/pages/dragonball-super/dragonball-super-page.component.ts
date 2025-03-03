b2c6e66c677fd980b58d7539278b2e924d513687
Giovanni Cortazar <manuelgiozar04@gmail.com>
2025-03-02 22:01:10 -0600

    feat: Organización de ficheros y se implementa nuevo componente

---
+++ bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -0,0 +1,63 @@
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
+@Component({
+  selector: 'app-dragonball-super',
+  // imports: [NgClass],
+  templateUrl: './dragonball-super-page.component.html',
+})
+export class DragonballSuperPageComponent {
+
+  name = signal('');
+  power = signal(0);
+
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 10000 },
+    { id: 2, name: 'Vegeta', power: 9000 },
+    { id: 3, name: 'Gohan', power: 8000 },
+    { id: 4, name: 'Trunks', power: 7000 },
+    { id: 5, name: 'Piccolo', power: 6000 },
+    { id: 6, name: 'Bulma', power: 5000 },
+    { id: 7, name: 'Krillin', power: 4000 },
+    { id: 8, name: 'Picollo', power: 3000 },
+    { id: 9, name: 'Chichi', power: 2000 },
+    { id: 10, name: 'Yamcha', power: 1000 }
+  ]);
+
+  addCharacter() {
+    console.log(this.name(), this.power());
+
+    if(!this.name() || !this.power() || this.power() <= 0) {
+      return;
+    }
+
+    const newCharacter = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    };
+
+    this.characters.update(characters => [...characters, newCharacter]);
+
+    this.resetFields();
+
+  }
+
+  resetFields(){
+    this.name.set('');
+    this.power.set(0);
+
+  }
+
+  // powerClasses = computed(() => {
+  //   return {
+  //     'text-danger': true
+  //   }
+  // })
+
+}
--- bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -9,25 +9,15 @@ interface Character {
 @Component({
   selector: 'app-dragonball',
   // imports: [NgClass],
-  templateUrl: './dragonball.component.html',
-  styleUrl: './dragonball.component.css'
+  templateUrl: './dragonball-page.component.html',
 })
-export class DragonballComponent {
+export class DragonballPageComponent {
 
-  name = signal('Goku');
-  power = signal(10000);
+  name = signal('');
+  power = signal(0);
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 10000 },
-    { id: 2, name: 'Vegeta', power: 9000 },
-    { id: 3, name: 'Gohan', power: 8000 },
-    { id: 4, name: 'Trunks', power: 7000 },
-    { id: 5, name: 'Piccolo', power: 6000 },
-    { id: 6, name: 'Bulma', power: 5000 },
-    { id: 7, name: 'Krillin', power: 4000 },
-    { id: 8, name: 'Picollo', power: 3000 },
-    { id: 9, name: 'Chichi', power: 2000 },
-    { id: 10, name: 'Yamcha', power: 1000 }
   ]);
 
   addCharacter() {
